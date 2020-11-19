f958a77c34d013561ee6faca75ceac5ef2277295
Junbin <zjbthomas@gmail.com>
2020-11-19 11:00:24 -0800

    updated data.py

--- python/data.py
+++ python/data.py
@@ -4,11 +4,11 @@ import re
 # parameters
 RESULT_BASE = 'E:\\eDocs\\PhD\\Y1S1\\ELEC523\\Project\\results\\'
 DATASETS = ['cjdata', 'brats'] # cjdata; brats;
-METHODS = ['fcm', 'flicm'] # otsu; fcm; flicm;
+METHODS = ['otsu', 'fcm', 'flicm'] # otsu; fcm; flicm;
 CNUMS = [4, 5]
 
 # constants
-TYPES = ['t1', 't2', 't1ce']
+TYPES = ['flair', 't1', 't2', 't1ce']
 
 for d in DATASETS:
     for m in METHODS:
@@ -87,30 +87,30 @@ for d in DATASETS:
                             time_sum = time_sum + float(lines[0 + offset].replace('Time: ', '').replace('\n', ''))
                             time_proc_sum = time_proc_sum + float(lines[1 + 2 * offset].replace('Time (skull stripped): ', '').replace('\n', ''))
 
-                            # Dice (lines 5/3, 10/8, and 15/13)
-                            dice_sum = dice_sum + float(lines[2 + 2 * offset].replace('Dice: ', '').replace('\n', ''))
-                            dice_proc_sum = dice_proc_sum + float(lines[7 + 2 * offset].replace('Dice (skull stripped): ', '').replace('\n', ''))
-                            dice_mask_sum = dice_mask_sum + float(lines[12 + 2 * offset].replace('Dice (skull stripped on mask): ', '').replace('\n', ''))
+                            # Dice (lines 7/5, 12/10, and 17/15)
+                            dice_sum = dice_sum + float(lines[4 + 2 * offset].replace('Dice: ', '').replace('\n', ''))
+                            dice_proc_sum = dice_proc_sum + float(lines[9 + 2 * offset].replace('Dice (skull stripped): ', '').replace('\n', ''))
+                            dice_mask_sum = dice_mask_sum + float(lines[14 + 2 * offset].replace('Dice (skull stripped on mask): ', '').replace('\n', ''))
 
-                            # Jaccard (lines 6/4, 11/9, 16/14)
-                            jaccard_sum = jaccard_sum + float(lines[3 + 2 * offset].replace('Jaccard: ', '').replace('\n', ''))
-                            jaccard_proc_sum = jaccard_proc_sum + float(lines[8 + 2 * offset].replace('Jaccard (skull stripped): ', '').replace('\n', ''))
-                            jaccard_mask_sum = jaccard_mask_sum + float(lines[13 + 2 * offset].replace('Jaccard (skull stripped on mask): ', '').replace('\n', ''))
+                            # Jaccard (lines 8/6, 13/11, 18/16)
+                            jaccard_sum = jaccard_sum + float(lines[5 + 2 * offset].replace('Jaccard: ', '').replace('\n', ''))
+                            jaccard_proc_sum = jaccard_proc_sum + float(lines[10 + 2 * offset].replace('Jaccard (skull stripped): ', '').replace('\n', ''))
+                            jaccard_mask_sum = jaccard_mask_sum + float(lines[15 + 2 * offset].replace('Jaccard (skull stripped on mask): ', '').replace('\n', ''))
 
-                            # statistics (lines 7/5, 12/10, 17/15)
-                            match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[4 + 2 * offset])
+                            # statistics (lines 9/7, 14/12, 19/17)
+                            match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[6 + 2 * offset])
                             TP = TP + int(match.group(1))
                             TN = TN + int(match.group(2))
                             FP = FP + int(match.group(3))
                             FN = FN + int(match.group(4))
 
-                            match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[9 + 2 * offset])
+                            match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[11 + 2 * offset])
                             TP_proc = TP_proc + int(match.group(1))
                             TN_proc = TN_proc + int(match.group(2))
                             FP_proc = FP_proc + int(match.group(3))
                             FN_proc = FN_proc + int(match.group(4))
 
-                            match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[14 + 2 * offset])
+                            match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[16 + 2 * offset])
                             TP_mask = TP_mask + int(match.group(1))
                             TN_mask = TN_mask + int(match.group(2))
                             FP_mask = FP_mask + int(match.group(3))
@@ -211,10 +211,16 @@ for d in DATASETS:
                                     lines = fin.readlines()
                                     fin.close()
 
-                                    # offset for FCM-based
+                                    # global offset for FCM-based
                                     offset = 0
                                     if (m is 'fcm' or m is 'flicm'):
                                         offset = 1
+
+                                    # mask offset for FCM-based
+                                    mask_offset = 0
+                                    if (m is 'fcm' or m is 'flicm'):
+                                        match = re.search(r'mask ([0-9]+)', lines[3 + 12 * c])
+                                        mask_offset = 12 * (int(match.group(1)) - 1)
 
                                     # iteration (line 1)
                                     if (offset > 0):
@@ -224,28 +230,28 @@ for d in DATASETS:
                                     # time (line 2/1)
                                     time_sum[t_i] = time_sum[t_i] + float(lines[0 + offset].replace('Time: ', '').replace('\n', ''))
 
-                                    # Dice (line 3/2)
-                                    dice_sum[t_i] = dice_sum[t_i] + float(lines[1 + offset].replace('Dice: ', '').replace('\n', ''))
+                                    # Dice (line 5/4 with mask offset)
+                                    dice_sum[t_i] = dice_sum[t_i] + float(lines[3 + offset + mask_offset].replace('Dice: ', '').replace('\n', ''))
 
-                                    # Jaccard (line 4/3)
-                                    jaccard_sum[t_i] = jaccard_sum[t_i] + float(lines[2 + offset].replace('Jaccard: ', '').replace('\n', ''))
+                                    # Jaccard (line 6/5 with mask offset)
+                                    jaccard_sum[t_i] = jaccard_sum[t_i] + float(lines[4 + offset + mask_offset].replace('Jaccard: ', '').replace('\n', ''))
 
-                                    # statistics (line 5/4)
-                                    match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[3 + offset])
+                                    # statistics (line 7/6 with mask offset)
+                                    match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[5 + offset + mask_offset])
                                     TP[t_i] = TP[t_i] + int(match.group(1))
                                     TN[t_i] = TN[t_i] + int(match.group(2))
                                     FP[t_i] = FP[t_i] + int(match.group(3))
                                     FN[t_i] = FN[t_i] + int(match.group(4))
 
                                     if (t_val is 't1' or t_val is 't1ce' or t_val is 't2'):
-                                        # Dice for type (line 8/7)
-                                        dice_t_sum[t_i] = dice_t_sum[t_i] + float(lines[6 + offset].replace('Dice (', '').replace(t_val + '): ', '').replace('\n', ''))
+                                        # Dice for type (line 10/9 with mask offset)
+                                        dice_t_sum[t_i] = dice_t_sum[t_i] + float(lines[8 + offset + mask_offset].replace('Dice (', '').replace(t_val + '): ', '').replace('\n', ''))
 
-                                        # Jaccard for type (line 9/8)
-                                        jaccard_t_sum[t_i] = jaccard_t_sum[t_i] + float(lines[7+ offset].replace('Jaccard (', '').replace(t_val + '): ', '').replace('\n', ''))
+                                        # Jaccard for type (line 11/10 with mask offset)
+                                        jaccard_t_sum[t_i] = jaccard_t_sum[t_i] + float(lines[9 + offset + mask_offset].replace('Jaccard (', '').replace(t_val + '): ', '').replace('\n', ''))
 
-                                        # statistics for type (line 10/9)
-                                        match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[8 + offset])
+                                        # statistics for type (line 12/11 with mask offset)
+                                        match = re.search(r'TP = ([0-9]+), TN = ([0-9]+), FP = ([0-9]+), FN = ([0-9]+)', lines[10 + offset + mask_offset])
                                         TP_t[t_i] = TP_t[t_i] + int(match.group(1))
                                         TN_t[t_i] = TN_t[t_i] + int(match.group(2))
                                         FP_t[t_i] = FP_t[t_i] + int(match.group(3))
